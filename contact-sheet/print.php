f47371c1c1defe3cfaf50a544082689aa655dbb7
dcfvg <qwsx@dcfvg.com>
2013-11-24 00:40:20 +0100

    use common functions

--- contact-sheet/print.php
+++ contact-sheet/print.php
@@ -1,33 +1,9 @@
 <?php
+//include('../krumo/class.krumo.php'); // debug only
+include("../functions.php");
 
-// include('../krumo/class.krumo.php'); // debug only
-
-$dir_assets = "../assets/"; // media location
-$dir_sets = "sets/";        // image collection ( sets ) location
-$code_type = "code128";     // codebar type ( symbologies )
-
-function contact($dir_assets,$dir_sets,$code_type){
-  
-  $img_sets = glob($dir_assets.$dir_sets."*"); 
-  
-  foreach ($img_sets as $id_set => $img_set){
-   
-   $imgs = glob($img_set."/*.*");
-   
-   foreach ($imgs as $id_img => $img) {
-     
-      $i++;
-      $code = str_pad( $i, 2, "0", STR_PAD_LEFT); // barcode 
-      
-      $html .= '
-      <p>
-        <img class="pic" src="'.$img.'" alt="">
-        <img class="code" src="barcode.php?text='.$code.'&codetype='.$code_type.'" alt="">
-      </p>';
-   }
-  }
-  return $html;
-}
+$path =  "../assets/sets/";
+$f_sets =  list_files($path);
 
 ?>
 <!DOCTYPE html>
@@ -39,7 +15,7 @@ function contact($dir_assets,$dir_sets,$code_type){
   </head>
   <body>
   <div class="sheet">
-    <?php contact($dir_assets,$dir_sets,$code_type);  ?>
+    <?php echo gen_contact($f_sets,0);  ?>
   </div>
   </body>
 </html>
