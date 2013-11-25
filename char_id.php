c3b782a54a4456c60a62f8f95cb2fd5161dc7ac4
dcfvg <qwsx@dcfvg.com>
2013-11-25 11:57:59 +0100

    move char_id testing script to tools

--- char_id.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include 'krumo/class.krumo.php';
-include 'functions.php';
-
-//file_put_contents("assets/id_cache.txt", serialize(gen_unique_ids(2)));
-//krumo(get_id(50,"assets/id_cache.txt"));
-
-?>
---
+++ tools/char_id.php
@@ -0,0 +1,8 @@
+<?php 
+include 'krumo/class.krumo.php';
+include '../functions.php';
+
+file_put_contents("assets/id_cache.txt", serialize(gen_unique_ids(2)));
+krumo(get_id(50,"assets/id_cache.txt"));
+
+?>
