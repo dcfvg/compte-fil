ea3a1a03877afa21398c520bcb6169d8ae73efe6
dcfvg <qwsx@dcfvg.com>
2013-11-25 15:54:43 +0100

    + jQueryhotkeys

--- reader.php
+++ reader.php
@@ -7,14 +7,15 @@
   </head>
   <body>
   	  	
+
+    <ol id="result" reversed="reversed"></ul>
+    
+    <video id="my_camera" autoplay="autoplay"></video>
     <form action="call.php" method="post" id="searchForm">
       <input id="code_input" name="s" value="">
     </form>
-    <ul id="result"></ul>
-    
-    <video id="my_camera" autoplay="autoplay"></video>
-    
     <script src="js/jquery-1.10.2.min.js" type="text/javascript" charset="utf-8"></script>
+    <script src="js/jquery.hotkeys/jquery.hotkeys.js"></script>
     <script src="js/reader.js" type="text/javascript" charset="utf-8"></script>
   </body>
 </html>
