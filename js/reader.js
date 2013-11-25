f109033dd51130b92997fed11c45ab33438a4049
dcfvg <qwsx@dcfvg.com>
2013-11-25 12:58:53 +0100

    camera implementation

--- js/reader.js
+++ js/reader.js
@@ -1,9 +1,14 @@
 $(function() {
+  var vid_h=1080,vid_w=1920;
 
   // events
-
   $( "html" ).keypress(function(event) {
-    if ( event.which == 103 ) thumbMode();
+    console.log(event.which);
+    
+    if ( event.which == 103 ) thumbMode();      // g
+    if ( event.which == 99 )  toogle_camode();  // c
+    if ( event.which == 115 ) take_snapshot();  // s
+    
   });
   $( "#searchForm" ).submit(function( event ) {
     event.preventDefault();
@@ -11,7 +16,6 @@ $(function() {
   });
   
   // functions
-  
   function scanmode(){
     // s field ready
     $("#code_input").focus();
@@ -36,7 +40,46 @@ $(function() {
       scanmode();
     });
   }
+  function init_camera(){
+    
+		navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+		
+		window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
 
-  scanmode();
+		if (navigator.getUserMedia && window.URL) {
+			var video = document.getElementById('my_camera');
+
+			navigator.getUserMedia({
+				"audio": false,
+				"video": { "mandatory": { "minWidth": vid_h, "minHeight": vid_h } }
+			},
+			function(stream) { // got access, attach stream to video
+				video.src = window.URL.createObjectURL( stream ) || stream;
+			});
+		}
+		else {
+			alert("getUserMedia not supported on your machine!");
+		}
+  }
+  function toogle_camode(){
+    $( "#my_camera" ).toggleClass( "on" );
+  }
+  function take_snapshot(){
+		// take snapshot and get image data
+		var video = document.getElementById('my_camera');
+		
+		var canvas = document.createElement('canvas');
+		canvas.width = vid_w;
+		canvas.height = vid_h;
+		var context = canvas.getContext('2d');
+		
+		context.drawImage(video, 0, 0, vid_w, vid_h);
+		var data_uri = canvas.toDataURL('image/jpeg', 1.0 );
+		
+		// display results in page
+		$( "#result" ).prepend('<li class="slide" style="background-image:url('+data_uri+');"></li>');
+	}
   
+  scanmode();
+  init_camera();
 })
--- reader.php
+++ reader.php
@@ -6,10 +6,14 @@
     <link rel="stylesheet" href="css/screen.css">
   </head>
   <body>
+  	  	
     <form action="call.php" method="post" id="searchForm">
       <input id="code_input" name="s" value="">
     </form>
     <ul id="result"></ul>
+    
+    <video id="my_camera" autoplay="autoplay"></video>
+    
     <script src="js/jquery-1.10.2.min.js" type="text/javascript" charset="utf-8"></script>
     <script src="js/reader.js" type="text/javascript" charset="utf-8"></script>
   </body>
