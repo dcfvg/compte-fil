93b4842d347abaf1b22d9655ad005ec22b4655f7
dcfvg <qwsx@dcfvg.com>
2013-11-25 15:55:14 +0100

    get error code if no anwser

--- call.php
+++ call.php
@@ -5,5 +5,8 @@ date_default_timezone_set('Europe/Paris');
 $path =  "assets/sets/";
 $f_sets =  list_files($path);    
 
+$img = $f_sets[0][$_POST["s"]-1];
+
+if($img == "") echo 'none';
+else echo '<li class="slide" style="background-image:url('.$img.');">'.date('h:i:s').'</li>';
 ?>
-<li class="slide" style="background-image:url(<?php echo $f_sets[0][$_POST["s"]-1] ?>);"> <?php echo date('h:i:s'); ?></li>
