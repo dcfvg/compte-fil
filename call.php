3d539429ac5c741c0835809a4fa85f4a5a6fc228
dcfvg <qwsx@dcfvg.com>
2013-11-25 01:14:25 +0100

    + time on new image

--- call.php
+++ call.php
@@ -1,9 +1,9 @@
 <?php
 include("functions.php");
-
+date_default_timezone_set('Europe/Paris');
 
 $path =  "assets/sets/";
 $f_sets =  list_files($path);    
 
 ?>
-<li class="slide" style="background-image:url(<?php echo $f_sets[0][$_POST["s"]] ?>);"></li>
+<li class="slide" style="background-image:url(<?php echo $f_sets[0][$_POST["s"]-1] ?>);"> <?php echo date('h:i:s'); ?></li>
