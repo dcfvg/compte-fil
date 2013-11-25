b276a7ea93c4996f63bc778b9ee64e34cec90872
dcfvg <qwsx@dcfvg.com>
2013-11-25 11:59:02 +0100

    clean reader.js

--- js/reader.js
+++ js/reader.js
@@ -1,32 +1,42 @@
 $(function() {
-  
-    $( "body" ).keypress(function(event) {
-      $("#code_input").focus();
-      
-      if ( event.which == 113 ) {
-          event.preventDefault();
-          $( "body" ).toggleClass( "mini" )
-       }
-      console.log( event.which );
-    });
-  
-    $("#code_input").focus();
-    $( "#searchForm" ).submit(function( event ) {
 
+  // events
+
+  $( "html" ).keypress(function(event) {
+    if ( event.which == 103 ) thumbMode();
+  });
+  $( "#searchForm" ).submit(function( event ) {
     event.preventDefault();
-
-    var $form = $( this ),
-    term = $form.find( "input[name='s']" ).val(),
+    get_media($( this ));
+  });
+  
+  // functions
+  
+  function scanmode(){
+    // s field ready
+    $("#code_input").focus();
+    $("#code_input").val('');
+  }
+  function thumbMode(){
+    // fs images to tumbnails
+    $( "body" ).toggleClass( "mini" );
+  }
+  function get_media($form){
+    
+    var term = $form.find( "input[name='s']" ).val(),
     url = $form.attr( "action" );
     
-    $(document).attr('title',term );
+    console.log('scanned_id :: '+term);
+    $(document).attr('title',term ); 
     
     var posting = $.post( url, { s: term } );
     
     posting.done(function( data ) {
       $( "#result" ).prepend( data );
-      $("#code_input").focus();
-      $form.find( "input[name='s']" ).val('');
+      scanmode();
     });
-  });
+  }
+
+  scanmode();
+  
 })
