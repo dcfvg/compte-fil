901184c77181fc5d1b6e82db609eb03f2d3daf95
dcfvg <qwsx@dcfvg.com>
2013-11-25 11:58:26 +0100

    renaming tool folder

---
+++ tools/print.php
@@ -0,0 +1,21 @@
+<?php
+include('krumo/class.krumo.php'); // debug only
+include("../functions.php");
+
+$path =  "../assets/sets/";
+$f_sets =  list_files($path);
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css/screen.css">
+  </head>
+  <body>
+  <div class="sheet">
+    <?php echo gen_contact($f_sets,0);  ?>
+  </div>
+  </body>
+</html>
