69d83d22f3a7648c6cb67ca12154a0ee7ddc6bd0
dcfvg <qwsx@dcfvg.com>
2013-12-01 22:42:04 +0100

    update call search patern

--- call.php
+++ call.php
@@ -6,7 +6,7 @@ $assets =  "assets/";
 
 if(isset($_POST["code"])) {
   $code = str_sort($_POST["code"]);
-  $fs = glob($assets."sets/*/www-1920/$code*");
+  $fs = glob($assets."sets/*/www-1920/*_$code*");
 
   echo $fs[0];
 }
