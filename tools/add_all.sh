daf3d5430dc189ad6d34badc44a2806a962de463
dcfvg <qwsx@dcfvg.com>
2014-06-16 18:16:49 +0200

    update glob patern

--- tools/add_all.sh
+++ tools/add_all.sh
@@ -1,6 +1,6 @@
 presetspath="/Users/benoit/Scripts/custom/compte-fil/assets/pre-sets"
 
-for dir in `find $presetspath -iname "7*" -type d`
+for dir in `find $presetspath -iname "*_*" -type d`
 do
   bash add_set.sh $dir
 done
